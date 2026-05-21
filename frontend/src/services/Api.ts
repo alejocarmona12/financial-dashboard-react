2b73efe6145dd84986acd6de382067bbab9c3f65
alejo carmona <alejocarmona224@gmail.com>
2026-05-21 20:00:28 -0300

    agrego scripts en backend

--- frontend/src/services/Api.ts
+++ frontend/src/services/Api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+// Detecta automáticamente si estás en Netlify o en tu computadora
+const API_URL = import.meta.env.PROD 
+  ? "https://onrender.com" // REEMPLAZÁ ESTO con la URL de tu Backend desplegado
+  : "http://localhost:4000/api"; 
+
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: API_URL,
 });
 
 export default api;
